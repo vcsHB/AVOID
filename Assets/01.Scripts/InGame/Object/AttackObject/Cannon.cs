f0066fcabc96eb4e9005f8b5d6b8c61722131d49
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-05-31 00:54:31 +0900

    feat: Target Area

--- Assets/01.Scripts/InGame/Object/AttackObject/Cannon.cs
+++ Assets/01.Scripts/InGame/Object/AttackObject/Cannon.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cannon : FieldObject
 {
-    
-    private float _currentTime;
+    [Header("Cannon Setting")] 
     [SerializeField] private float _attackCoolTime;
+    [SerializeField] private float _shootPower;
+    [SerializeField] private float _playerDetectRadius = 7f;
+    [SerializeField] private LayerMask _targetLayer;
 
-    private Transform _cannonHeadTrm;
+    private float _currentTime = 0;
+    private bool _isCoolTimed;
+
+    [Header("Essential Setting")]
+    [SerializeField] private Transform _cannonHeadTrm;
     private Transform _gunTipTrm;
+    [SerializeField] private TargetArea _targetArea;
 
+    
+    private bool _isPlayerDetected;
+    
 
     protected override void Awake()
     {
@@ -18,6 +29,25 @@ public class Cannon : FieldObject
         _gunTipTrm = _cannonHeadTrm.Find("GunTip");
     }
 
+    private void Update()
+    {
+        if (!_isCoolTimed)
+        {
+            _currentTime += Time.deltaTime;
+        }
+        
+    }
+
+    private void DetectPlayer()
+    {
+        
+    }
+
+    private void Fire()
+    {
+        
+    }
+    
 
     public override void ResetItem()
     {
