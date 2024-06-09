4d057ad8f935b25cbcde6f644a3efbf1798fe2fe
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-06-09 23:14:17 +0900

    feat: TextDisplay Show

--- Assets/01.Scripts/InGame/Object/TextDisplay.cs
+++ Assets/01.Scripts/InGame/Object/TextDisplay.cs
@@ -1,16 +1,34 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
-public class TextDisplay : MonoBehaviour
+public class TextDisplay : LogicObject
 {
     private TextMeshPro _tmp;
 
-    [Header("Settings")] [SerializeField] private int a;
+    [Header("Settings")] 
+    [SerializeField] private float _showDuration = 1f;
+    [SerializeField] private Color _color;
+    private Color _offColor;
     
     private void Awake()
     {
         _tmp = transform.Find("Text").GetComponent<TextMeshPro>();
+        _offColor = _color;
+        _offColor.a = 0;
     }
+
+    private void Start()
+    {
+        logicSolvedEvent.AddListener(ShowTextDisplay);
+    }
+
+    public void ShowTextDisplay()
+    {
+        _tmp.color = _offColor;
+        _tmp.DOColor(_color, _showDuration);
+    }
+
     
     
 }
